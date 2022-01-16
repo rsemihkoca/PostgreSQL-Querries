a1f85e8c455f12b1c836389d1bb1f52a2e3d623f
Rıza Semih Koca <82779464+rsemihkoca@users.noreply.github.com>
2022-01-16 19:57:32 +0300

    Update HW_2.sql

--- HW_2.sql
+++ HW_2.sql
@@ -1,1 +1,8 @@
-"\nSELECT * FROM film WHERE replacement_cost between 12.99 AND 16.99;\nSELECT first_name,last_name FROM actOR WHERE first_name in('Penelope','Nick','Ed');\nSELECT * FROM film WHERE replacement_cost in (12.99,15.99,28.99);\n/*\nfilm tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)\n.actOR tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)\nfilm tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)\n*/\n"
+SELECT * FROM film WHERE replacement_cost between 12.99 AND 16.99;
+SELECT first_name,last_name FROM actOR WHERE first_name in('Penelope','Nick','Ed');
+SELECT * FROM film WHERE replacement_cost in (12.99,15.99,28.99);
+/*
+film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
+.actOR tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
+film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
+*/
