04da7729f595449e368355d69ab81026c48fe754
Rıza Semih Koca <82779464+rsemihkoca@users.noreply.github.com>
2022-01-16 19:58:04 +0300

    Update HW_2.sql

--- HW_2.sql
+++ HW_2.sql
@@ -1,6 +1,6 @@
 SELECT * FROM film WHERE replacement_cost between 12.99 AND 16.99;
-SELECT first_name,last_name FROM actOR WHERE first_name in('Penelope','Nick','Ed');
-SELECT * FROM film WHERE replacement_cost in (12.99,15.99,28.99);
+SELECT first_name,last_name FROM actOR WHERE first_name IN('Penelope','Nick','Ed');
+SELECT * FROM film WHERE replacement_cost IN (12.99,15.99,28.99);
 /*
 film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
 .actOR tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
